96a4176f594e8791d8b848115e36d7b51c6abdb7
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2014-11-03 18:43:16 +0100

    Successfully load spreadsheet layers

--- SpreadsheetLayersPlugin.py
+++ SpreadsheetLayersPlugin.py
@@ -21,15 +21,15 @@
  ***************************************************************************/
 """
 import os.path
-from PyQt4.QtCore import QSettings, QTranslator, qVersion, QCoreApplication
-from PyQt4.QtGui import QAction, QIcon
+from qgis.core import QgsVectorLayer, QgsMapLayerRegistry
+from PyQt4 import QtCore, QtGui
 # Initialize Qt resources from file resources.py
 from .ui import resources_rc
 # Import the code for the dialog
 from .widgets.SpreadsheetLayers_dialog import SpreadsheetLayersPluginDialog
 
 
-class SpreadsheetLayersPlugin:
+class SpreadsheetLayersPlugin(QtCore.QObject):
     """QGIS Plugin Implementation."""
 
     def __init__(self, iface):
@@ -40,151 +40,46 @@ class SpreadsheetLayersPlugin:
             application at run time.
         :type iface: QgsInterface
         """
+        super(SpreadsheetLayersPlugin, self).__init__()
         # Save reference to the QGIS interface
         self.iface = iface
         # initialize plugin directory
         self.plugin_dir = os.path.dirname(__file__)
         # initialize locale
-        locale = QSettings().value('locale/userLocale')[0:2]
+        locale = QtCore.QSettings().value('locale/userLocale')[0:2]
         locale_path = os.path.join(
             self.plugin_dir,
             'i18n',
             'SpreadsheetLayersPlugin_{}.qm'.format(locale))
 
         if os.path.exists(locale_path):
-            self.translator = QTranslator()
+            self.translator = QtCore.QTranslator()
             self.translator.load(locale_path)
 
-            if qVersion() > '4.3.3':
-                QCoreApplication.installTranslator(self.translator)
-
-        # Create the dialog (after translation) and keep reference
-        self.dlg = SpreadsheetLayersPluginDialog()
-
-        # Declare instance attributes
-        self.actions = []
-        self.menu = self.tr(u'&Spreadsheet Layers')
-        # TODO: We are going to let the user set this up in a future iteration
-        self.toolbar = self.iface.addToolBar(u'SpreadsheetLayersPlugin')
-        self.toolbar.setObjectName(u'SpreadsheetLayersPlugin')
-
-    # noinspection PyMethodMayBeStatic
-    def tr(self, message):
-        """Get the translation for a string using Qt translation API.
-
-        We implement this ourselves since we do not inherit QObject.
-
-        :param message: String for translation.
-        :type message: str, QString
-
-        :returns: Translated version of message.
-        :rtype: QString
-        """
-        # noinspection PyTypeChecker,PyArgumentList,PyCallByClass
-        return QCoreApplication.translate('SpreadsheetLayersPlugin', message)
-
-
-    def add_action(
-        self,
-        icon_path,
-        text,
-        callback,
-        enabled_flag=True,
-        add_to_menu=True,
-        add_to_toolbar=True,
-        status_tip=None,
-        whats_this=None,
-        parent=None):
-        """Add a toolbar icon to the InaSAFE toolbar.
-
-        :param icon_path: Path to the icon for this action. Can be a resource
-            path (e.g. ':/plugins/foo/bar.png') or a normal file system path.
-        :type icon_path: str
-
-        :param text: Text that should be shown in menu items for this action.
-        :type text: str
-
-        :param callback: Function to be called when the action is triggered.
-        :type callback: function
-
-        :param enabled_flag: A flag indicating if the action should be enabled
-            by default. Defaults to True.
-        :type enabled_flag: bool
-
-        :param add_to_menu: Flag indicating whether the action should also
-            be added to the menu. Defaults to True.
-        :type add_to_menu: bool
-
-        :param add_to_toolbar: Flag indicating whether the action should also
-            be added to the toolbar. Defaults to True.
-        :type add_to_toolbar: bool
-
-        :param status_tip: Optional text to show in a popup when mouse pointer
-            hovers over the action.
-        :type status_tip: str
-
-        :param parent: Parent widget for the new action. Defaults None.
-        :type parent: QWidget
-
-        :param whats_this: Optional text to show in the status bar when the
-            mouse pointer hovers over the action.
-
-        :returns: The action that was created. Note that the action is also
-            added to self.actions list.
-        :rtype: QAction
-        """
-
-        icon = QIcon(icon_path)
-        action = QAction(icon, text, parent)
-        action.triggered.connect(callback)
-        action.setEnabled(enabled_flag)
-
-        if status_tip is not None:
-            action.setStatusTip(status_tip)
-
-        if whats_this is not None:
-            action.setWhatsThis(whats_this)
-
-        if add_to_toolbar:
-            self.toolbar.addAction(action)
-
-        if add_to_menu:
-            self.iface.addPluginToMenu(
-                self.menu,
-                action)
-
-        self.actions.append(action)
-
-        return action
+            if QtCore.qVersion() > '4.3.3':
+                QtCore.QCoreApplication.installTranslator(self.translator)
 
     def initGui(self):
-        """Create the menu entries and toolbar icons inside the QGIS GUI."""
-
-        icon_path = ':/plugins/SpreadsheetLayersPlugin/icon/icon.png'
-        self.add_action(
-            icon_path,
-            text=self.tr(u'Spreadsheet Layers'),
-            callback=self.run,
-            parent=self.iface.mainWindow())
-
+        self.action = QtGui.QAction(
+            QtGui.QIcon(':/plugins/SpreadsheetLayers/icon/spreadsheet7.svg'),
+            self.tr("Add spreadsheet layer"),
+            self)
+        self.action.triggered.connect(self.showDialog)
+        self.iface.addLayerMenu().addAction(self.action)
+        self.iface.layerToolBar().addAction(self.action)
 
     def unload(self):
-        """Removes the plugin menu item and icon from QGIS GUI."""
-        for action in self.actions:
-            self.iface.removePluginMenu(
-                self.tr(u'&Spreadsheet Layers'),
-                action)
-            self.iface.removeToolBarIcon(action)
+        if hasattr(self, 'action'):
+            self.iface.addLayerMenu().removeAction(self.action)
+            self.iface.layerToolBar().removeAction(self.action)
 
-
-    def run(self):
-        """Run method that performs all the real work"""
-        # show the dialog
-        self.dlg.show()
-        # Run the dialog event loop
-        result = self.dlg.exec_()
-        # See if OK was pressed
-        if result:
-            # Do something useful here - delete the line containing pass and
-            # substitute with your code.
-            pass
+    def showDialog(self):
+        dlg = SpreadsheetLayersPluginDialog(self.iface.mainWindow())
+        dlg.show()
+        if dlg.exec_():
+            layer = QgsVectorLayer(dlg.vrtPath(), dlg.layerName(), 'ogr')
+            if not layer.isValid():
+                print "Layer failed to load"
+            else:
+                QgsMapLayerRegistry.instance().addMapLayer(layer)
+        dlg.deleteLater()
--- widgets/SpreadsheetLayers_dialog.py
+++ widgets/SpreadsheetLayers_dialog.py
@@ -22,15 +22,425 @@
 """
 
 import os
-
-from PyQt4 import QtGui, uic
-
+from osgeo import gdal, ogr
+from qgis.gui import QgsMessageBar, QgsGenericProjectionSelector
+from PyQt4 import QtCore, QtGui, QtXml
 from ..ui.ui_SpreadsheetLayers_dialog import Ui_SpreadsheetLayersPluginDialogBase
 
 
-class SpreadsheetLayersPluginDialog(QtGui.QDialog):
+class QOgrFieldModel(QtCore.QAbstractListModel):
+
+    def __init__(self, layer, parent=None):
+        super(QOgrFieldModel, self).__init__(parent)
+        self.layer = layer
+        self.layerDefn = self.layer.GetLayerDefn()
+
+    def rowCount(self, parent):
+        if parent.isValid():
+            return 0
+        return self.layerDefn.GetFieldCount()
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            fieldDefn = self.layerDefn.GetFieldDefn(index.row())
+            if fieldDefn is None:
+                return ''
+            return fieldDefn.GetNameRef()
+
+        if role == QtCore.Qt.ItemDataRole:
+            return self.layerDefn.GetFieldDefn(index.row())
+
+
+class QOgrTableModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, layer, parent=None, maxRowCount=None):
+        super(QOgrTableModel, self).__init__(parent)
+        self.layer = layer
+        self.layerDefn = self.layer.GetLayerDefn()
+        self.maxRowCount = maxRowCount
+
+    def rowCount(self, parent):
+        if parent.isValid():
+            return 0
+        return min(self.layer.GetFeatureCount(), self.maxRowCount)
+
+    def columnCount(self, parent):
+        if parent.isValid():
+            return 0
+        return self.layerDefn.GetFieldCount()
+
+    def data(self, index, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            self.layer.SetNextByIndex(index.row())
+            feature = self.layer.GetNextFeature()
+            if feature is None:
+                return None
+
+            iField = index.column()
+            fieldDefn = self.layerDefn.GetFieldDefn(iField)
+            if fieldDefn.GetType() == ogr.OFTInteger:
+                return feature.GetFieldAsInteger(iField)
+
+            elif fieldDefn.GetType() == ogr.OFTReal:
+                return feature.GetFieldAsDouble(iField)
+
+            elif fieldDefn.GetType() == ogr.OFTString:
+                return feature.GetFieldAsString(iField)
+
+            else:
+                return feature.GetFieldAsString(iField)
+
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        if role == QtCore.Qt.DisplayRole:
+            if orientation == QtCore.Qt.Horizontal:
+                fieldDef = self.layerDefn.GetFieldDefn(section)
+                return fieldDef.GetNameRef()
+
+            if orientation == QtCore.Qt.Vertical:
+                return section
+
+
+class SpreadsheetLayersPluginDialog(QtGui.QDialog, Ui_SpreadsheetLayersPluginDialogBase):
+
+    pluginKey = 'SpreadsheetLayers'
+    sampleRowCount = 20
+
     def __init__(self, parent=None):
         """Constructor."""
         super(SpreadsheetLayersPluginDialog, self).__init__(parent)
-        self.ui = Ui_SpreadsheetLayersPluginDialogBase()
-        self.ui.setupUi(self)
+        self.setupUi(self)
+
+        self.dataSource = None
+        self.layer = None
+
+        self.messageBar = QgsMessageBar(self)
+        self.layout().insertWidget(0, self.messageBar)
+
+    def info(self, msg):
+        self.messageBar.pushMessage(msg, QgsMessageBar.INFO, 5)
+
+    def warning(self, msg):
+        self.messageBar.pushMessage(msg, QgsMessageBar.WARNING, 5)
+
+    def filePath(self):
+        return self.filePathEdit.text()
+
+    def setFilePath(self, path):
+        self.filePathEdit.setText(path)
+
+    @QtCore.pyqtSlot(name='on_filePathEdit_editingFinished')
+    def on_filePathEdit_editingFinished(self):
+        self.afterOpenFile()
+
+    @QtCore.pyqtSlot(name='on_filePathButton_clicked')
+    def on_filePathButton_clicked(self):
+        settings = QtCore.QSettings()
+        s = QtGui.QFileDialog.getOpenFileName(
+            self,
+            str(self.tr("Choose a spreadsheet file to open")),
+            str(settings.value(self.pluginKey + "/directory", "./")),
+            str(self.tr("Spreadsheet files") + " (*.ods *.xls *.xlsx);;"
+                + self.tr("GDAL Virtual Format") + " (*.vrt);;"
+                + self.tr("All files") + " (* *.*)".format()))
+        if s == '':
+            return
+        settings.setValue(self.pluginKey + "/directory", os.path.dirname(s))
+        self.filePathEdit.setText(s)
+
+        self.afterOpenFile()
+
+    def afterOpenFile(self):
+        self.openDataSource()
+        self.updateSheetBox()
+        self.readVrt()
+
+    def layerName(self):
+        return self.layerNameEdit.text()
+
+    def setLayerName(self, name):
+        self.layerNameEdit.setText(name)
+
+    def closeDataSource(self):
+        if self.dataSource is not None:
+            self.dataSource = None
+            self.updateSheetBox()
+
+    def openDataSource(self):
+        self.closeDataSource()
+
+        filePath = self.filePath()
+        finfo = QtCore.QFileInfo(filePath)
+        if not finfo.exists():
+            return
+
+        self.layerNameEdit.setText(finfo.completeBaseName())
+
+        dataSource = ogr.Open(filePath, 0)
+        if dataSource is None:
+            self.messageBar.pushMessage('Could not open {}'.format(filePath),
+                                        QgsMessageBar.WARNING, 5)
+        self.dataSource = dataSource
+
+    def sheet(self):
+        return self.sheetBox.currentText()
+
+    def setSheet(self, sheetName):
+        index = self.sheetBox.findText(sheetName)
+        if index == -1:
+            return False
+        self.sheetBox.setCurrentIndex(index)
+        return True
+
+    def updateSheetBox(self):
+        self.sheetBox.clear()
+        dataSource = self.dataSource
+        if dataSource is None:
+            return
+
+        for i in xrange(0, dataSource.GetLayerCount()):
+            layer = dataSource.GetLayer(i)
+            self.sheetBox.addItem(layer.GetName(), layer)
+
+    @QtCore.pyqtSlot(int, name='on_sheetBox_currentIndexChanged')
+    def on_sheetBox_currentIndexChanged(self, index):
+        if index is None:
+            self.layer = None
+        else:
+            self.layer = self.sheetBox.itemData(index)
+        self.updateFieldBoxes()
+        self.updateSampleView()
+
+    def xField(self):
+        return self.xFieldBox.currentText()
+
+    def setXField(self, fieldName):
+        index = self.xFieldBox.findText(fieldName)
+        if index == -1:
+            return False
+        self.xFieldBox.setCurrentIndex(index)
+        return True
+
+    def yField(self):
+        return self.yFieldBox.currentText()
+
+    def setYField(self, fieldName):
+        index = self.yFieldBox.findText(fieldName)
+        if index == -1:
+            return False
+        self.yFieldBox.setCurrentIndex(index)
+        return True
+
+    def updateFieldBoxes(self):
+        if self.layer is None:
+            self.xFieldBox.clear()
+            return
+
+        model = QOgrFieldModel(self.layer, parent=self)
+
+        xField = self.xField()
+        yField = self.xField()
+
+        self.xFieldBox.setModel(model)
+        self.yFieldBox.setModel(model)
+
+        self.xFieldBox.setCurrentIndex(self.xFieldBox.findText(xField))
+        self.yFieldBox.setCurrentIndex(self.yFieldBox.findText(yField))
+
+        if self.xField() != '' and self.yField() != '':
+            return
+
+        self.tryFields("longitude", "latitude")
+        self.tryFields("lon", "lat")
+        self.tryFields("x", "y")
+
+    def tryFields(self, xName, yName):
+        if self.xField() == '':
+            for i in xrange(0, self.xFieldBox.count()):
+                xField = self.xFieldBox.itemText(i)
+                if xField.lower().find(xName.lower()) != -1:
+                    self.xFieldBox.setCurrentIndex(i)
+                    break;
+
+        if self.yField() == '':
+            for i in xrange(0, self.yFieldBox.count()):
+                yField = self.yFieldBox.itemText(i)
+                if yField.lower().find(yName.lower()) != -1:
+                    self.yFieldBox.setCurrentIndex(i)
+                    break;
+
+    def crs(self):
+        return self.crsEdit.text()
+
+    def setCrs(self, crs):
+        self.crsEdit.setText(crs)
+
+    @QtCore.pyqtSlot(name='on_crsButton_clicked')
+    def on_crsButton_clicked(self):
+        dlg = QgsGenericProjectionSelector(self)
+        dlg.setMessage('Select CRS')
+        dlg.setSelectedAuthId(self.crsEdit.text())
+        if dlg.exec_():
+            self.crsEdit.setText(dlg.selectedAuthId())
+
+    def updateSampleView(self):
+        layer = self.layer
+        if layer is None:
+            self.sampleView.setModel(None)
+            return
+
+        model = QOgrTableModel(layer, parent=self,
+                               maxRowCount=self.sampleRowCount)
+        self.sampleView.setModel(model)
+
+    def validate(self):
+        try:
+            if self.dataSource is None:
+                raise ValueError(self.tr("Please select an input file"))
+
+            if self.layer is None:
+                raise ValueError(self.tr("Please select a sheet"))
+
+            if self.xField == '':
+                raise ValueError(self.tr("Please select an x field"))
+
+            if self.yField == '':
+                raise ValueError(self.tr("Please select an y field"))
+
+        except ValueError as e:
+            self.messageBar.pushMessage(unicode(e), QgsMessageBar.WARNING, 5)
+            return False
+
+        return True
+
+    def vrtPath(self):
+        return '{}.vrt'.format(self.filePath())
+
+    def readVrt(self):
+        vrtPath = self.vrtPath()
+
+        if not os.path.exists(vrtPath):
+            return False
+
+        file = QtCore.QFile(vrtPath)
+        if not file.open(QtCore.QIODevice.ReadOnly | QtCore. QIODevice.Text):
+            self.warning("Impossible to open VRT file {}".format(vrtPath))
+            return False
+
+        stream = QtCore.QXmlStreamReader(file)
+
+        stream.readNextStartElement()
+        if stream.name() == "OGRVRTDataSource":
+
+            stream.readNextStartElement()
+            if stream.name() == "OGRVRTLayer":
+                self.setLayerName(stream.attributes().value("name"))
+
+                while stream.readNextStartElement():
+                    if stream.name() == "SrcDataSource":
+                        # do nothing : datasource should be already set
+                        pass
+
+                    elif stream.name() == "SrcLayer":
+                        text = stream.readElementText()
+                        self.setSheet(text)
+
+                    elif stream.name() == "GeometryType":
+                        pass
+
+                    elif stream.name() == "LayerSRS":
+                        text = stream.readElementText()
+                        self.setCrs(text)
+
+                    elif stream.name() == "GeometryField":
+                        self.setXField(stream.attributes().value("x"))
+                        self.setYField(stream.attributes().value("y"))
+
+                    if not stream.isEndElement():
+                        stream.skipCurrentElement()
+
+            stream.skipCurrentElement()
+
+        stream.skipCurrentElement()
+
+        file.close()
+
+        self.info("Existing VRT file has been loaded")
+        return True
+
+    def prepareVrt(self):
+        buffer = QtCore.QBuffer()
+        buffer.open(QtCore.QBuffer.ReadWrite)
+
+        stream = QtCore.QXmlStreamWriter(buffer)
+        stream.setAutoFormatting(True)
+        stream.writeStartDocument()
+        stream.writeStartElement("OGRVRTDataSource")
+
+        stream.writeStartElement("OGRVRTLayer")
+        stream.writeAttribute("name", self.layerName())
+
+        stream.writeStartElement("SrcDataSource")
+        stream.writeAttribute("relativeToVRT", "1")
+        stream.writeCharacters(os.path.basename(self.filePath()))
+        stream.writeEndElement()
+
+        stream.writeStartElement("SrcLayer")
+        stream.writeCharacters(self.sheet())
+        stream.writeEndElement()
+
+        stream.writeStartElement("GeometryType")
+        stream.writeCharacters("wkbPoint")
+        stream.writeEndElement()
+
+        if self.crs():
+            stream.writeStartElement("LayerSRS")
+            stream.writeCharacters(self.crs())
+            stream.writeEndElement()
+
+        stream.writeStartElement("GeometryField")
+        stream.writeAttribute("encoding", "PointFromColumns")
+        stream.writeAttribute("x", self.xField())
+        stream.writeAttribute("y", self.yField())
+        stream.writeEndElement()
+
+        stream.writeEndElement() # OGRVRTLayer
+        stream.writeEndElement() # OGRVRTDataSource
+        stream.writeEndDocument()
+
+        buffer.reset()
+        content = buffer.readAll()
+        buffer.close
+
+        return content
+
+    def writeVrt(self):
+        content = self.prepareVrt()
+
+        vrtPath = self.vrtPath()
+        file = QtCore.QFile(vrtPath)
+        if file.exists():
+            msgBox = QtGui.QMessageBox()
+            msgBox.setText("The file {} already exist.".format(vrtPath))
+            msgBox.setInformativeText("Do you want to overwrite ?");
+            msgBox.setStandardButtons(QtGui.QMessageBox.Ok | QtGui.QMessageBox.Cancel)
+            msgBox.setDefaultButton(QtGui.QMessageBox.Cancel)
+            ret = msgBox.exec_()
+            if ret == QtGui.QMessageBox.Cancel:
+                return False
+
+        if not file.open(QtCore.QIODevice.ReadWrite | QtCore. QIODevice.Text):
+            self.warning("Impossible to open VRT file {}".format(vrtPath))
+            return False
+
+        file.write(content)
+        file.close()
+        return True
+
+    def accept(self, *args, **kwargs):
+        if not self.validate():
+            return False
+
+        if not self.writeVrt():
+            return False
+
+        return super(SpreadsheetLayersPluginDialog, self).accept(*args, **kwargs)
