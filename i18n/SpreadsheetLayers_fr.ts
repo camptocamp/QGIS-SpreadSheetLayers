04b6b651edabb4afabd0fb7735af93c1864dd7ba
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2014-11-06 16:23:04 +0100

    Add french translations strings

---
+++ i18n/SpreadsheetLayers_fr.ts
@@ -0,0 +1,108 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="fr" sourcelanguage="en">
+<context>
+    <name>SpreadsheetLayersPlugin</name>
+    <message>
+        <location filename="../SpreadsheetLayersPlugin.py" line="63"/>
+        <source>Add spreadsheet layer</source>
+        <translation>Ajouter une couche feuille de calcul</translation>
+    </message>
+</context>
+<context>
+    <name>SpreadsheetLayersPluginDialog</name>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="140"/>
+        <source>Choose a spreadsheet file to open</source>
+        <translation>Choisissez une feuille de calcul à ouvrir</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="140"/>
+        <source>Spreadsheet files</source>
+        <translation>Fichiers feuille de calcul</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="140"/>
+        <source>GDAL Virtual Format</source>
+        <translation>GDAL Virtual Format</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="140"/>
+        <source>All files</source>
+        <translation>Tous les fichiers</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="288"/>
+        <source>Please select an input file</source>
+        <translation>Veuillez sélectionner un fichier</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="291"/>
+        <source>Please select a sheet</source>
+        <translation>Veuillez sélectionner une feuille</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="294"/>
+        <source>Please select an x field</source>
+        <translation>Veuillez sélectionner un champ x</translation>
+    </message>
+    <message>
+        <location filename="../widgets/SpreadsheetLayers_dialog.py" line="297"/>
+        <source>Please select an y field</source>
+        <translation>Veuillez sélectionner un champ y</translation>
+    </message>
+</context>
+<context>
+    <name>SpreadsheetLayersPluginDialogBase</name>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="177"/>
+        <source>Create a Layer from a Spreadsheet File</source>
+        <translation>Créer une couche depuis un fichier feuille de calcul</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="178"/>
+        <source>File Name</source>
+        <translation>Fichier</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="179"/>
+        <source>Browse...</source>
+        <translation>Parcourir...</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="180"/>
+        <source>Layer name</source>
+        <translation>Nom de la couche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="181"/>
+        <source>Sheet</source>
+        <translation>Feuille</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="182"/>
+        <source>Field options</source>
+        <translation>Options de champs</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="185"/>
+        <source>Reference system</source>
+        <translation>Système de référence</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="186"/>
+        <source>Select</source>
+        <translation>Sélectionner</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="183"/>
+        <source>X field</source>
+        <translation>Champ X</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_SpreadsheetLayers_dialog.py" line="184"/>
+        <source>Y field</source>
+        <translation>Champ Y</translation>
+    </message>
+</context>
+</TS>
