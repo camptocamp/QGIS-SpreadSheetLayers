bc6933e01b43723b7262aa4a657ad4184d25c728
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2020-04-28 10:13:23 +0200

    Update plugin_upload.py

--- plugin_upload.py
+++ plugin_upload.py
@@ -1,23 +1,19 @@
-#!/usr/bin/env python
-# coding=utf-8
-"""This script uploads a plugin package on the server.
+#!/usr/bin/env python3
+"""This script uploads a plugin package to the plugin repository.
         Authors: A. Pasotti, V. Picavet
         git sha              : $TemplateVCSFormat
 """
-from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
-from builtins import input
 
+import os
 import sys
 import getpass
 import xmlrpc.client
 from optparse import OptionParser
 
 # Configuration
-PROTOCOL = 'http'
+PROTOCOL = 'https'
 SERVER = 'plugins.qgis.org'
-PORT = '80'
+PORT = '443'
 ENDPOINT = '/plugins/RPC2/'
 VERBOSE = False
 
@@ -28,42 +24,32 @@ def main(parameters, arguments):
     :param parameters: Command line parameters.
     :param arguments: Command line arguments.
     """
-    address = "%s://%s:%s@%s:%s%s" % (
-        PROTOCOL,
-        parameters.username,
-        parameters.password,
-        parameters.server,
-        parameters.port,
-        ENDPOINT)
-    # fix_print_with_import
+    address = "{protocol}://{username}:{password}@{server}:{port}{endpoint}".format(
+        protocol=PROTOCOL,
+        username=parameters.username,
+        password=parameters.password,
+        server=parameters.server,
+        port=parameters.port,
+        endpoint=ENDPOINT)
     print("Connecting to: %s" % hide_password(address))
 
     server = xmlrpc.client.ServerProxy(address, verbose=VERBOSE)
 
     try:
-        plugin_id, version_id = server.plugin.upload(
-            xmlrpc.client.Binary(open(arguments[0]).read()))
-        # fix_print_with_import
+        with open(arguments[0], 'rb') as handle:
+            plugin_id, version_id = server.plugin.upload(
+                xmlrpc.client.Binary(handle.read()))
         print("Plugin ID: %s" % plugin_id)
-        # fix_print_with_import
         print("Version ID: %s" % version_id)
     except xmlrpc.client.ProtocolError as err:
-        # fix_print_with_import
         print("A protocol error occurred")
-        # fix_print_with_import
         print("URL: %s" % hide_password(err.url, 0))
-        # fix_print_with_import
         print("HTTP/HTTPS headers: %s" % err.headers)
-        # fix_print_with_import
         print("Error code: %d" % err.errcode)
-        # fix_print_with_import
         print("Error message: %s" % err.errmsg)
     except xmlrpc.client.Fault as err:
-        # fix_print_with_import
         print("A fault occurred")
-        # fix_print_with_import
         print("Fault code: %d" % err.faultCode)
-        # fix_print_with_import
         print("Fault string: %s" % err.faultString)
 
 
@@ -88,10 +74,14 @@ if __name__ == "__main__":
     parser = OptionParser(usage="%prog [options] plugin.zip")
     parser.add_option(
         "-w", "--password", dest="password",
-        help="Password for plugin site", metavar="******")
+        help="Password for plugin site. "
+             "You can use environment variable 'PLUGIN_UPLOAD_PASSWORD', "
+             "or the password will be prompted on runtime.", metavar="******")
     parser.add_option(
         "-u", "--username", dest="username",
-        help="Username of plugin site", metavar="user")
+        help="Username of plugin site. "
+             "You can use environment variable 'PLUGIN_UPLOAD_USERNAME', "
+             "or the username will be prompted on runtime.", metavar="user")
     parser.add_option(
         "-p", "--port", dest="port",
         help="Server port to connect to", metavar="80")
@@ -100,7 +90,6 @@ if __name__ == "__main__":
         help="Specify server name", metavar="plugins.qgis.org")
     options, args = parser.parse_args()
     if len(args) != 1:
-        # fix_print_with_import
         print("Please specify zip file.\n")
         parser.print_help()
         sys.exit(1)
@@ -109,16 +98,26 @@ if __name__ == "__main__":
     if not options.port:
         options.port = PORT
     if not options.username:
-        # interactive mode
-        username = getpass.getuser()
-        # fix_print_with_import
-        print("Please enter user name [%s] :" % username, end=' ')
-        res = input()
-        if res != "":
-            options.username = res
-        else:
+        username = os.environ.get('PLUGIN_UPLOAD_USERNAME')
+        if username:
+            # environment variable
             options.username = username
+        else:
+            # interactive mode
+            username = getpass.getuser()
+            print("Please enter user name [%s] :" % username)
+
+            res = input()
+            if res != "":
+                options.username = res
+            else:
+                options.username = username
     if not options.password:
-        # interactive mode
-        options.password = getpass.getpass()
+        password = os.environ.get('PLUGIN_UPLOAD_PASSWORD')
+        if password:
+            # environment variable
+            options.password = password
+        else:
+            # interactive mode
+            options.password = getpass.getpass()
     main(options, args)
