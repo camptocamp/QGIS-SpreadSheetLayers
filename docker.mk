1cd632f4c7e15c45f9ecce654eed024cc6659da8
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2023-04-22 13:58:17 +0200

    Fix transifex push and pull

--- docker.mk
+++ docker.mk
@@ -10,6 +10,18 @@ help: ## Display this help message
 	@echo "Possible targets:"
 	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | sort | awk 'BEGIN {FS = ":.*?## "}; {printf "    %-20s%s\n", $$1, $$2}'
 
+build: doc transcompile
+
+
+#################
+# DOCUMENTATION
+#################
+
+doc: ## Generate documentation files
+	make -C help html
+	mkdir -p $(PLUGINNAME)/help/
+	cp -r help/build/html/* $(PLUGINNAME)/help/
+
 
 ########
 # Lint #
@@ -47,3 +59,43 @@ pytest:  ## Run tests using pytest
 .PHONY: coverage
 coverage: ## Display coverage report
 	coverage report -m
+
+
+###############
+# TRANSLATION
+###############
+
+tx-pull: ## Pull translations from transifex using tx client
+tx-pull: $(HOME)/.transifexrc
+	mkdir -p $(PLUGINNAME)/i18n
+	tx pull --all
+
+tx-push: ## Push translations on transifex using tx client
+tx-push: transup
+	tx push --source
+
+transup: ## Update translation catalogs
+transup: $(PLUGINNAME)/i18n/SpreadsheetLayers_en.ts
+transup: help/locale/index.pot
+
+.INTERMEDIATE: $(PLUGINNAME)/SpreadsheetLayers.pro
+.PHONY: $(PLUGINNAME)/SpreadsheetLayers.pro
+$(PLUGINNAME)/SpreadsheetLayers.pro:
+	echo "SOURCES = $(SOURCES)" > $@
+	echo "FORMS = $(FORMS)" >> $@
+	echo "TRANSLATIONS = i18n/SpreadsheetLayers_en.ts" >> $@
+	echo "CODECFORTR = UTF-8" >> $@
+	echo "CODECFORSRC = UTF-8" >> $@
+
+.INTERMEDIATE: $(PLUGINNAME)/i18n/SpreadsheetLayers_en.ts
+$(PLUGINNAME)/i18n/SpreadsheetLayers_en.ts: $(PLUGINNAME)/SpreadsheetLayers.pro
+	mkdir -p $(dir $@)
+	pylupdate5 -noobsolete $(PLUGINNAME)/SpreadsheetLayers.pro
+
+.INTERMEDIATE: help/locale/index.pot
+help/locale/index.pot: venv
+	make -C help gettext
+
+transcompile: ## Compile Qt .ts translation files into .qm binary format
+transcompile:
+	lrelease $(shell find -name *.ts)
